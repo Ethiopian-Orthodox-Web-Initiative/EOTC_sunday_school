f4376df2452b8abf4825d1fc2a7e1aa48637440e
tinbit25 <tinbitelias25@gmail.com>
2024-11-20 16:37:54 +0300

    starting frontend structure

--- sunday-church-app/frontend/src/pages/_app.tsx
+++ sunday-church-app/frontend/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 // pages/_app.tsx
 import React from 'react';
 import { AppProps } from 'next/app';
-import '../styles/globals.css'; // Import global styles here
+import '../styles/globals.css'; 
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
